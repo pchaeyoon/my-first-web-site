100e2df23a88265b6a4b040d511dfeacf1e64b66
pchaeyoon <114378720+pchaeyoon@users.noreply.github.com>
2022-12-05 14:51:54 +0900

    Add files via upload

---
+++ master.js
@@ -0,0 +1,42 @@
+var Links = {
+  setColor :function (color){
+    var alist = document.querySelectorAll('a');
+      var i = 0;
+      while (i < alist.length) {
+        alist[i].style.color = color;
+        i = i + 1;
+  }
+}
+};
+
+var Body = {
+  setColor :function(color){
+  document.querySelector('body').style.color=color;
+},
+  setBGColor :function(color){
+  document.querySelector('body').style.backgroundColor=color;
+  }
+};
+
+
+
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+
+
+  if(self.value == 'night'){
+    Body.setBGColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+
+    Links.setColor('powderblue');
+
+  } else {
+    Body.setBGColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('blue');
+
+  }
+}
